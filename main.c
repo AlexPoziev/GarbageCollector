2de9135d6d00ffce946d694cc756139bda2dc753
Aleksey Poziev <poziev24@gmail.com>
2022-10-09 13:29:41 +0300

    add Object struct, add stack and primal operations

--- main.c
+++ main.c
@@ -1,11 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_STACK_SIZE 256
+
 typedef enum
 {
-    INT;
-    TWIN;
+    INT,
+    TWIN
 }oType;
+
+typedef struct sObject
+{
+    oType type;
+
+    union
+    {
+        int value;
+
+        struct {
+            struct sObject *head;
+            struct sObject *title
+        };
+    };
+}Object;
+
+typedef struct
+{
+    Object *stack[MAX_STACK_SIZE];
+    int stackSize;
+}vm;
+
+void push(vm *vm, Object *value)
+{
+    vm->stack[vm->stackSize] = value;
+}
+
+Object* pop(vm* vm)
+{
+    return vm->stackSize[--vm->stackSize];
+}
 
 
 
