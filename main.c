a26eab14b95bcb765eba3e001808b0fc8feea976
Aleksey Poziev <poziev24@gmail.com>
2022-10-09 17:54:26 +0300

    add marks

--- main.c
+++ main.c
@@ -12,14 +12,14 @@ typedef enum
 typedef struct sObject
 {
     oType type;
-
+    unsigned char marked;
     union
     {
         int value;
 
         struct {
             struct sObject *head;
-            struct sObject *title
+            struct sObject *tail
         };
     };
 }Object;
@@ -37,10 +37,60 @@ void push(vm *vm, Object *value)
 
 Object* pop(vm* vm)
 {
-    return vm->stackSize[--vm->stackSize];
+    return vm->stack[--vm->stackSize];
 }
 
+vm* newVm(void)
+{
+    vm *mainVm = (vm*)(malloc(sizeof(vm)));
+    mainVm->stackSize = 0;
+    return mainVm;
+}
 
+Object* newObject(oType type)
+{
+    Object *object = (Object*)(malloc(sizeof(Object)));
+    object->type = type;
+    return object;
+}
+
+void pushInt(vm* vm, int intV)
+{
+    Object *object = newObject(INT);
+    object->value = intV;
+    push(vm, object);
+}
+
+Object *pushTwin(vm* vm)
+{
+    Object *object = newObject(TWIN);
+    object->tail = pop(vm);
+    object->head = pop(vm);
+
+    push(vm, object);
+    return object;
+}
+
+void mark(Object* object)
+{
+    if (object->marked) return;
+
+    object->marked = 1;
+
+    if (object->type == TWIN)
+    {
+        mark(object->head);
+        mark(object->tail);
+    }
+}
+
+void markAll(vm *vm)
+{
+    for (int i = 0; i < vm->stackSize; ++i)
+    {
+        mark(vm->stack[i]);
+    }
+}
 
 int main()
 {
